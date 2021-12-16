23a150639e2d1f174a131a22f80ed0c4e173e091
saraafr6 <61750145+saraafr6@users.noreply.github.com>
2021-12-16 23:09:55 +0330

    step2

---
+++ exercism/high_scores.py
@@ -0,0 +1,12 @@
+def latest(scores):
+    return scores[-1]
+
+
+def personal_best(scores):
+    return max(scores)
+    
+
+
+def personal_top_three(scores):
+    x = sorted(scores, reverse=True)
+    return x[:3]
